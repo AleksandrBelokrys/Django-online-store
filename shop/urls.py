17c4d14adf28e0123cee9d6e06eb165f340fc1b3
Oleksandr <aleksandrbelokrys@gmail.com>
2021-08-07 12:41:16 +0300

    created signup.html

--- shop/urls.py
+++ shop/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('category/<slug:category_slug>', views.home, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
     path('product/', views.product, name='product'),
-    path('cart', views.cart, name='cart'),
+    path('cart', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
+    path('cart/remove_product/<int:product_id>', views.cart_remove_product, name='cart_remove_product'),
+    path('account/create/', views.signUpView, name='signup'),
+
 ]
